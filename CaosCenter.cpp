c39e7b471938e69ea12a66d7fed2dbb9fc2bc33f
Gustavo Henrique <Gustavohgbzxc123@gmail.com>
2024-05-12 11:53:55 -0300

    lista PC(incompleta)

--- CaosCenter.cpp
+++ CaosCenter.cpp
@@ -2,9 +2,49 @@
 
 using namespace std;
 
+struct Client {
+    int id;
+    int t;
+    int p;
+    int r;
+};
+
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
+	int n, somaTotal = 0;
+	bool ocupado = 0;
+	queue<Client> callCenter;
+	
+
+	cin >> n;
+
+	for(int i = 1; i < n + 1; i++){
+		int t, p, r;
+
+		cin >> t >> p >> r;
+		Client client;
+		client.id = i;
+		client.t = t;
+        client.p = p;
+        client.r = r;
+		callCenter.push(client);
+
+	}
+
+	for(int i = 0; i < n; i++){
+		if(!ocupado){
+			Client cliente = callCenter.front();
+			somaTotal += cliente.t;
+		}
+		
+	}
+
+	for(int i = 0; i < n; i++) {
+    Client cliente = callCenter.front();
+    callCenter.pop();
+	cout << cliente.id << " " << cliente.t << " " << cliente.p << " " << cliente.r << endl;
+	}
 
 	return 0;
 }
--- PapaiNoel.cpp
+++ PapaiNoel.cpp
@@ -1,12 +1,12 @@
 #include<bits/stdc++.h>
 
 using namespace std;
-typedef pair<long long int, long long int> pii;
+typedef pair<int, int> pii;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	long long int x, n, maior_pilha = 0;
+	int x, n, maior_pilha, aux;
 	char c;
 	stack<pii> saco;
 
@@ -19,6 +19,7 @@ int main(){
 			if(saco.empty())
 				maior_pilha = x;
 			else{
+				tie(aux, maior_pilha) = saco.top();
 				maior_pilha = max(x, saco.top().second);
 			}
 			saco.push({x, maior_pilha});
@@ -31,7 +32,7 @@ int main(){
 
 		}else if(c == 'R'){
 			if(!saco.empty())
-				saco.top();
+				saco.pop();
 		}
 	}
 
