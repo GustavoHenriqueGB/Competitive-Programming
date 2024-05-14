31bfe107aaf4b16ea9d323b78a2208606c2523e6
Gustavo Henrique <Gustavohgbzxc123@gmail.com>
2024-05-14 16:04:02 -0300

    caoscenter incompleta

--- CaosCenter.cpp
+++ CaosCenter.cpp
@@ -1,50 +1,49 @@
 #include<bits/stdc++.h>
 
 using namespace std;
+typedef tuple<int, int, int, int, int> ti;
 
-struct Client {
-    int id;
-    int t;
-    int p;
-    int r;
+struct ordenar{
+    bool operator()(ti a, ti b){
+        if(get<0>(a) == get<0>(b))
+            return get<1>(a) < get<1>(b);
+        else
+            return get<0>(a) < get<0>(b);
+    }
 };
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	int n, somaTotal = 0;
+	int n, livre = 0, tempoFinal = 0;
 	bool ocupado = 0;
-	queue<Client> callCenter;
+	priority_queue<ti, vector<ti>, ordenar> callCenter;
+
 	
 
 	cin >> n;
 
 	for(int i = 1; i < n + 1; i++){
 		int t, p, r;
-
 		cin >> t >> p >> r;
-		Client client;
-		client.id = i;
-		client.t = t;
-        client.p = p;
-        client.r = r;
-		callCenter.push(client);
+		callCenter.push({0, i, t, p, r});
+	}
+
+	while(!callCenter.empty()){
+		int tempoChegada, id, tempoEsperado, paciencia, retorno, tempoInicial;
+		tie(tempoChegada, id, tempoEsperado, paciencia, retorno) = callCenter.top();
+		callCenter.pop();
+		tempoInicial = tempoChegada + livre;
+		livre = tempoEsperado + tempoInicial;
+		if(tempoInicial < livre)
+			tempoFinal = tempoEsperado + tempoInicial;
+		else
+			callCenter.push({tempoChegada+paciencia+retorno, id, tempoEsperado, paciencia, retorno});
 
 	}
 
-	for(int i = 0; i < n; i++){
-		if(!ocupado){
-			Client cliente = callCenter.front();
-			somaTotal += cliente.t;
-		}
-		
-	}
-
-	for(int i = 0; i < n; i++) {
-    Client cliente = callCenter.front();
-    callCenter.pop();
-	cout << cliente.id << " " << cliente.t << " " << cliente.p << " " << cliente.r << endl;
-	}
+	cout << tempoFinal << "\n";
 
 	return 0;
+		
 }
