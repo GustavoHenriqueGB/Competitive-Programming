c32a429debd407ab4f416a3c4853efd71e01d661
Gustavo Henrique <Gustavohgbzxc123@gmail.com>
2024-05-29 17:19:36 -0300

    att

--- ErrosdeCompilacao.cpp
+++ ErrosdeCompilacao.cpp
@@ -1,4 +1,3 @@
-
 #include<bits/stdc++.h>
 
 using namespace std;
@@ -7,27 +6,33 @@ int main(){
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
 	int n;
-	long long int s1 = 0, s2 = 0, s3 = 0;
-		
+	set<int> erros;
+	vector<int> entradas;
+	stack<int> ans;
+
 	cin >> n;
-	
+
 	for(int i = 0; i < n; i++){
 		int aux;
 		cin >> aux;
-		s1 += aux;
+		entradas.push_back(aux)
 	}
-	for(int i = 0; i < n - 1; i++){
-		int aux;
-		cin >> aux;
-		s2 += aux;
-	}
+
+
+
 	for(int i = 0; i < n - 2; i++){
 		int aux;
 		cin >> aux;
-		s3 += aux;
-	}
+		if(!(erros.count(aux)))
+
+	}	
 	
-	cout << s1 - s2 << "\n" << s2 - s3 << "\n";
+	while(!ans.empty()){
+		int saida = ans.top();
+		ans.pop(s);
+		cout << saida << "\n";
+	}
 
 	return 0;
+		
 }
