d92f817f52c864c033ae9beee352ab1fe2f29daa
Gustavo Henrique <Gustavohgbzxc123@gmail.com>
2024-05-29 20:23:28 -0300

    Revert "att"
    This reverts commit c32a429debd407ab4f416a3c4853efd71e01d661.

--- ErrosdeCompilacao.cpp
+++ ErrosdeCompilacao.cpp
@@ -1,3 +1,4 @@
+
 #include<bits/stdc++.h>
 
 using namespace std;
@@ -6,33 +7,27 @@ int main(){
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
 	int n;
-	set<int> erros;
-	vector<int> entradas;
-	stack<int> ans;
-
+	long long int s1 = 0, s2 = 0, s3 = 0;
+		
 	cin >> n;
-
+	
 	for(int i = 0; i < n; i++){
 		int aux;
 		cin >> aux;
-		entradas.push_back(aux)
+		s1 += aux;
 	}
-
-
-
+	for(int i = 0; i < n - 1; i++){
+		int aux;
+		cin >> aux;
+		s2 += aux;
+	}
 	for(int i = 0; i < n - 2; i++){
 		int aux;
 		cin >> aux;
-		if(!(erros.count(aux)))
-
-	}	
-	
-	while(!ans.empty()){
-		int saida = ans.top();
-		ans.pop(s);
-		cout << saida << "\n";
+		s3 += aux;
 	}
+	
+	cout << s1 - s2 << "\n" << s2 - s3 << "\n";
 
 	return 0;
-		
 }
